bc63588a7f8fcb7edd19c92fbb64432abbd7333a
Wesley Araújo Vieira <wesley.contato@hotmail.com>
2022-04-06 20:01:02 -0300

    Configurações iniciais do projeto

---
+++ backend/src/test/java/com/github/wesleyav/apispringangularsample/ApiSpringAngularSampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.wesleyav.apispringangularsample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiSpringAngularSampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

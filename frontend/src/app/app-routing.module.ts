5d19af3c2eea0dedf164409dcdd3b78feb2ee817
Wesley Araújo Vieira <wesley.contato@hotmail.com>
2022-04-06 21:47:56 -0300

    Cria o Módulo de Cities e usa roteamento com Lazy Loading

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,10 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', pathMatch: 'full', redirectTo: 'cities' },
+  {
+    path: 'cities',
+    loadChildren: () =>
+      import('./cities/cities.module').then((m) => m.CitiesModule),
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
